08afca97750dfab23674367235fba118cf533af2
CzechVadim <czech_vadim@ukr.net>
2022-01-07 17:50:04 +0200

    REACT-ЭЛЕМЕНТЫ

--- src/card.js
+++ src/card.js
@@ -3,21 +3,23 @@ import ReactDOM from 'react-dom';   /* принимает ссылку на Reac
 
 
 /* КАРТОЧКА ПРОДУКТА */
-const image = React.createElement(   /* создание react елемента */
+const image = React.createElement(   /* создание react елемента img */
     "img",   /* type */
     {
     src: "https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640",   /* props */
     alt: "Tacos With Lime",   /* props */
     width: 640,   /* props */
     }
-  );
+);
+const title = React.createElement("h2", null, "Tacos With Lime");   /* создание react елемента h2 */
+const price = React.createElement("p", null, "Price: 10.99$");   /* создание react елемента p */
+const button = React.createElement("button", { type: "button" }, "Add to cart");   /* создание react елемента button */
   
-  const title = React.createElement("h2", null, "Tacos With Lime");
-  const price = React.createElement("p", null, "Price: 10.99$");
-  const button = React.createElement("button", { type: "button" }, "Add to cart");
+const productWithChildrenInProps = React.createElement(   /* создание react елемента div */
+  "div",
+  {
+  children: [image, title, price, button],   /* Для передачи детей используется свойство children */
+  }
+);
   
-  const productWithChildrenInProps = React.createElement("div", {
-    children: [image, title, price, button],
-  });
-  
-  ReactDOM.render(productWithChildrenInProps, document.querySelector('#root'));   /* рендер react элемента в DOM-дерево (первым передается что нужно зарендерить, а вторым куда зарендерить) */
+ReactDOM.render(productWithChildrenInProps, document.querySelector('#root'));   /* рендер react элемента в DOM-дерево (первым передается что нужно зарендерить, а вторым куда зарендерить) */
